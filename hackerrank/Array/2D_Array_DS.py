e4913ada1a6a1b411441abe206df4f82dd02b1f5
rlaxogud0909 <rlaxogud0909@gmail.com>
2021-12-31 14:47:52 +0900

    12/31 hackerrank  2D Arrray_DS

---
+++ hackerrank/Array/2D_Array_DS.py
@@ -0,0 +1,18 @@
+def hourglassSum(arr):
+    # Write your code here
+    sum = []
+
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+            
+    return max(sum)
+
+if __name__ == '__main__':
+    arr = []
+    
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = hourglassSum(arr)
+    
+    print(result)
